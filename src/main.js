2b17685a14831b526601ce03df73ecff5c4b51c3
cwyyyds <1013953614@qq.com>
2022-07-03 21:07:47 +0800

    初始化组件，父组件引入，css样式引入

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// base 基础样式放在上面
+import './assets/styles/base.css'
+import './assets/styles/index.css'
+
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app')
