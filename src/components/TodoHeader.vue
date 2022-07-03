0c6317c24eab08a0cd5cb65eb6151ec2df10defe
cwyyyds <1013953614@qq.com>
2022-07-03 21:23:17 +0800

    todo案例-添加任务

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -4,10 +4,31 @@
     <input id="toggle-all" class="toggle-all" type="checkbox" />
     <label for="toggle-all"></label>
     <!-- label 可以关联一个表单标签 -->
-    <input class="new-todo" placeholder="输入任务名称-回车确认" autofocus />
+    <input
+      class="new-todo"
+      placeholder="输入任务名称-回车确认"
+      autofocus
+      v-mode="task"
+      @keydown.enter="downFn"
+    />
   </header>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      task: '',
+    }
+  },
+  methods: {
+    downFn() {
+      if (this.task.trim().length === 0) {
+        alert('任务名不能为空')
+        return
+      }
+      this.$emit('create', this.task)
+    },
+  },
+}
 </script>
